f4563449c48a88a645814ebed606fa3fbd462199
differentsyntax <nothingreallyfaraway@gmail.com>
2020-08-13 03:18:08 -0400

    Homepage finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+        <div className = "App">
+          {/* <Navbar bg = "dark" /> */}
+          <div id = "page-body" >
+            <Switch>
+              <Route path = "/" component = {Home} exact />
+              <Route path = "/experience" component = {Home} exact/>
+              <Route path = "/projects" component = {Home} exact/>
+              <Route path = "/dragonhacks" component = {Home} exact/>
+              <Route path = "/really" component = {Home} exact/>
+              {/* <Route component = {Notfoundpage} /> */}
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
